b8ff741d3fa82fd2d7cf29b9a164aa48449483f5
Sasha Gunderak <o.gunderak@gmail.com>
2021-11-10 21:21:05 +0200

    + добрий вечір

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Добрий день");
+        System.out.println("Добрий вечір");
     }
 }
